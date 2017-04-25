c09d5baae4e11acebda2fd85db06209abe1e4a54
宁宁 <383514580@qq.com>
2017-04-25 15:24:56 +0800

    清除无用代码

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="view">
-        <a class="btn btn-block btn-primary" @click="popup">确定</a>
-        <v-notification></v-notification>        
+        <a class="btn btn-primary" @click="popup">确定</a>
+        <hr>
+
+        <v-button style="margin: 15px;" type="default" icon="check">确定</v-button>
+
+        <v-button style="margin: 15px;" type="primary" icon="check">确定</v-button>
+
+        <v-button style="margin: 15px;" type="success">确定</v-button>
+
+        <v-button style="margin: 15px;" type="danger">确定</v-button>
+
+        <v-button style="margin: 15px;" type="warning">确定</v-button>
+
+        <v-button style="margin: 15px;" type="disabled">确定</v-button>
+        
     </div>
 </template>
 <script>
-import VNotification from '../components/Notification/Main'
+import VButton from '../components/form/Button'
 
 export default {
     name: 'testView',
@@ -16,7 +29,14 @@ export default {
 
     methods: {
         popup() {
-            this.$notify('ID: ')
+            // this.$alert('我就说ID: ');
+            this.$alert('我就说?');
+
+            // this.$notify('通过CSS3,我们能够创建动画,这可以在许多网页中取代动画图片、Flash 动画以及 JavaScript...', {
+            //     title: '自定义标题',
+            //     holdTime: 500000,
+            //     type: 'warning'
+            // });
             // this.$confirm(1234567890, {
             //     holdTime: 100000
             // }).then(() => {}).catch(() => {});
@@ -24,7 +44,7 @@ export default {
     },
 
     components: {
-        VNotification
+        VButton
     }
 }
 </script>
